c145c35fd0376d52cdf2ee4bf765660446d1658a
kitaee <dlrlxo999@naver.com>
2024-07-19 22:23:38 +0900

    Scafford를 이용한 appBar 뼈대 만들기

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,17 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('홈'),
+      ),
+      body: Center(
+        child: Text('0',
+            style: TextStyle(
+              color: Colors.red,
+              fontSize: 70,
+            )),
+      ),
+    );
   }
 }
