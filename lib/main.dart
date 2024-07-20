1a6e161dfd002cb731f00756fee571084239ee3a
kitaee <dlrlxo999@naver.com>
2024-07-20 10:58:38 +0900

    화면별 파일 분리

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:untitled/my_home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,69 +18,6 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int count = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('홈'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('숫자',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 40,
-                )),
-            Text('$count',
-                style: TextStyle(
-                  color: Colors.red,
-                  fontSize: 70,
-                )),
-            ElevatedButton(
-              onPressed: () {
-                print("ElevatedButton");
-              },
-              child: Text('ElevatedButton'),
-            ),
-            TextButton(
-              onPressed: () {
-                print("TextButton");
-              },
-              child: Text('TextButton'),
-            ),
-            OutlinedButton(
-              onPressed: () {
-                print("OutlinedButton");
-              },
-              child: Text('OutlinedButton'),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            count++;
-          });
-        },
-        child: Icon(Icons.add),
-      ),
     );
   }
 }
---
+++ lib/my_home_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('홈'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('숫자',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 40,
+                )),
+            Text('$count',
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 70,
+                )),
+            ElevatedButton(
+              onPressed: () {
+                print("ElevatedButton");
+              },
+              child: Text('ElevatedButton'),
+            ),
+            TextButton(
+              onPressed: () {
+                print("TextButton");
+              },
+              child: Text('TextButton'),
+            ),
+            OutlinedButton(
+              onPressed: () {
+                print("OutlinedButton");
+              },
+              child: Text('OutlinedButton'),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            count++;
+          });
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
